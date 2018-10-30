91990c3525c0dd678990497e37b18b1674b97e16
djknit <djknit@gmail.com>
2018-10-29 19:07:44 -0500

    initial site files

---
+++ bamazonCustomer.js
@@ -0,0 +1,4 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const columnify = require("columnify");
+const chalk = require("chalk");
---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Toasted Oats Cereal (22 oz)", "Grocery", 2, 10),
+("80% Cacao Chocolate Bar (12 oz)", "Grocery", 1.89, 10),
+("Liquid Dish Soap (19.2 oz)", "Cleaning", 1.29, 30),
+("Clumping Cat Litter (35 lbs)", "Pet", 13.59, 5),
+("Large Pet Bed", "Pet", 29.99, 2),
+("Diapers: Size 4 (150 ct)", "Baby", 39.99, 6),
+("Diapers: Size 6 (150 ct)", "Baby", 41.99, 6),
+("Grain Free Organic Cat Food (4.9 lbs)", "Pet", 8.99, 5),
+("Liquid Laundry Detergent (180 oz)", "Cleaning", 7.99, 3),
+("Grain Free Organic Dog Food (35 lbs)", "Pet", 44.99, 4);
